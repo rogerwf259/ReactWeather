6e1ed35b595650811112914e9d76c2e82c043905
Roger <rogeliomer_2@hotmail.com>
2017-06-07 19:34:38 -0500

    fixed modal

--- src/components/ErrorModal.js
+++ src/components/ErrorModal.js
@@ -1,19 +1,11 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import ReactDOMServer from 'react-dom/server';
 
 class ErrorModal extends Component {
-  componentDidMount() {
-    var modal = new Foundation.Reveal($('#error-modal'));
-    modal.open();
-  }
-  static get defaultProps() {
-    return {
-      title: 'Oops'
-    }
-  }
-  render() {
-
-    var { title, message } = this.props;
-    return (
+  componentDidMount () {
+    var {title, message} = this.props;
+    var modalMarkup = (
       <div id="error-modal" className="reveal tiny text-center" data-reveal="">
         <h4>{title}</h4>
         <p>{message}</p>
@@ -23,6 +15,22 @@ class ErrorModal extends Component {
           </button>
         </p>
       </div>
+    );
+
+    var $modal = $(ReactDOMServer.renderToString(modalMarkup));
+    $(ReactDOM.findDOMNode(this)).html($modal);
+
+    var modal = new Foundation.Reveal($('#error-modal'));
+    modal.open();
+  }
+  static get defaultProps() {
+    return {
+      title: 'Oops'
+    }
+  }
+  render() {
+    return (
+      <div></div>
     );
   }
 }
